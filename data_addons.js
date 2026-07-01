59cacfc70665beb7a04c609651ccacfe5cc2652d
VOXZA-codex <nokosvoxza@gmail.com>
2026-07-01 12:39:49 +0700

    Add files via upload

---
+++ data_addons.js
@@ -0,0 +1,90 @@
+// DAFTAR SEMUA ADDON - SUDAH DIISI CONTOH
+const daftarAddon = [
+  {
+    id: 1,
+    nama: "Plants VS Zombie",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/owadwg.jpg",
+    linkUnduh: "https://www.mediafire.com/file/z1ma1wd8w3hizhe/PvZClassic499.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "4.42 MB",
+    deskripsi: "Addon yang menambah kan banyak mob dan tanaman yang sama persis seperti game plants vs zombie"
+  },
+  {
+    id: 2,
+    nama: "The Dawn Era",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/6650gt.jpg",
+    linkUnduh: "https://www.mediafire.com/file/s551hjy2ctcnlj3/The+Dawn+Era+1.2.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "18.28 MB",
+    deskripsi: "Tambah berbagai jenis dinosaurs yang bisa dijinakkan, dipelihara, dan dibawa keliling dunia."
+  },
+  {
+    id: 3,
+    nama: "Clasic Shadow",
+    pembuat: "Voxz Official",
+    kategori: "Texture",
+    gambar: "https://files.catbox.moe/rs7opp.jpg",
+    linkUnduh: "https://www.mediafire.com/file/ui948mtyihp0eto/Classic+Shadows+(resources).mcpack/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "19.48 MB",
+    deskripsi: "no caption."
+  },
+  {
+    id: 4,
+    nama: "Realistic Seasons",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/vkhcdl.jpg",
+    linkUnduh: "https://www.mediafire.com/file/llj7f2qw4llotp3/REALISTIC_SEASONS%2521_Add-On_%2528addon%2529.mcaddon/file",
+    ukuran: "2.69 MB",
+    deskripsi: "Add-on ysng menambah kan beberapa musim di minecraft."
+  },
+  {
+    id: 5,
+    nama: "Demon Slayer Weapons",
+    pembuat: "Voxz Official",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/61p71c.jpg",
+    linkUnduh: "https://www.mediafire.com/file/uaacnn6hulytf41/Demon_Slayer_Weapons_%2528world_template%2529.mctemplate/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "1.8 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 6,
+    nama: "The Ultimate Animal MashUp",
+    pembuat: "Voxz Official",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/uvgb8r.jpg",
+    linkUnduh: "https://www.mediafire.com/file/qftakva3bgoll20/The+Ultimate+Animal+Mashup+By+Kynzo.zip/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "86.79 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 7,
+    nama: "Mini Soldiers",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/6t0fc4.jpg",
+    linkUnduh: "https://www.mediafire.com/file/cqpwqv96w9gs9r1/Mini+soldier+Add-on.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "7.04 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 8,
+    nama: "Weapons Armors",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/hdrnx1.jpg",
+    linkUnduh: "https://www.mediafire.com/file/u4c2pm7iiiaz734/Weapons+++Armors+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "1.33 MB",
+    deskripsi: "No Captions."
+  }
+];
