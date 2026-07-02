560e77b81dd169be9f75020c9e3aac3688cedebb
VOXZA-codex <nokosvoxza@gmail.com>
2026-07-02 17:52:00 +0700

    Add files via upload

---
+++ data_addons.js
@@ -0,0 +1,222 @@
+// DAFTAR SEMUA ADDON - SUDAH DIISI CONTOH
+const daftarAddon = [
+  {
+    id: 1,
+    nama: "Plants VS Zombie",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/owadwg.jpg",
+    linkUnduh: "https://www.mediafire.com/file/z1ma1wd8w3hizhe/PvZClassic499.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "4.42 MB",
+    deskripsi: "Addon yang menambah kan banyak mob dan tanaman yang sama persis seperti game plants vs zombie"
+  },
+  {
+    id: 2,
+    nama: "The Dawn Era",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/6650gt.jpg",
+    linkUnduh: "https://www.mediafire.com/file/s551hjy2ctcnlj3/The+Dawn+Era+1.2.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "18.28 MB",
+    deskripsi: "Tambah berbagai jenis dinosaurs yang bisa dijinakkan, dipelihara, dan dibawa keliling dunia."
+  },
+  {
+    id: 3,
+    nama: "Clasic Shadow",
+    pembuat: "Voxz Official",
+    kategori: "Texture",
+    gambar: "https://files.catbox.moe/rs7opp.jpg",
+    linkUnduh: "https://www.mediafire.com/file/ui948mtyihp0eto/Classic+Shadows+(resources).mcpack/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "19.48 MB",
+    deskripsi: "no caption."
+  },
+  {
+    id: 4,
+    nama: "Realistic Seasons",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/vkhcdl.jpg",
+    linkUnduh: "https://www.mediafire.com/file/llj7f2qw4llotp3/REALISTIC_SEASONS%2521_Add-On_%2528addon%2529.mcaddon/file",
+    ukuran: "2.69 MB",
+    deskripsi: "Add-on ysng menambah kan beberapa musim di minecraft."
+  },
+  {
+    id: 5,
+    nama: "Demon Slayer Weapons",
+    pembuat: "Voxz Official",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/61p71c.jpg",
+    linkUnduh: "https://www.mediafire.com/file/uaacnn6hulytf41/Demon_Slayer_Weapons_%2528world_template%2529.mctemplate/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "1.8 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 6,
+    nama: "The Ultimate Animal MashUp",
+    pembuat: "Voxz Official",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/uvgb8r.jpg",
+    linkUnduh: "https://www.mediafire.com/file/qftakva3bgoll20/The+Ultimate+Animal+Mashup+By+Kynzo.zip/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "86.79 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 7,
+    nama: "Mini Soldiers",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/6t0fc4.jpg",
+    linkUnduh: "https://www.mediafire.com/file/cqpwqv96w9gs9r1/Mini+soldier+Add-on.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "7.04 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 8,
+    nama: "Weapons Armors",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/hdrnx1.jpg",
+    linkUnduh: "https://www.mediafire.com/file/u4c2pm7iiiaz734/Weapons+++Armors+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "1.33 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 9,
+    nama: "Village Expansion",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/ktccch.jpg",
+    linkUnduh: "https://www.mediafire.com/file/sbav4beb0zldn13/Village+Expansion+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "5.76 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 10,
+    nama: "Utility Tools",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/l2xzgq.jpg",
+    linkUnduh: "https://www.mediafire.com/file/ycsptwfd0xrpt3l/Utility+Tools+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "3.41 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 11,
+    nama: "Productive Bees",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/4c1amj.jpg",
+    linkUnduh: "https://www.mediafire.com/file/8k5oj5fh5gkqxbl/Productive+Bees+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "3.9 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 12,
+    nama: "Mermaid Life",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/bl86ba.jpg",
+    linkUnduh: "https://www.mediafire.com/file/bwppiuc3135j4a8/Mermaid+Life+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "3.62 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 13,
+    nama: "Medieval City Defence",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/1eie7c.jpg",
+    linkUnduh: "https://www.mediafire.com/file/dgqjgpyzomrcyjr/Medieval+City+Defence+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "530.34 KB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 14,
+    nama: "Fragments Of The Overeorld",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/1j4rnl.jpg",
+    linkUnduh: "https://www.mediafire.com/file/hj1pv6zsmkya9sr/Fragments+of+the+Overworld+Add-On+(addon).mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "18.44 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 15,
+    nama: "Archeologeist",
+    pembuat: "Voxz Official",
+    kategori: "Texture",
+    gambar: "https://files.catbox.moe/42cx8t.jpg",
+    linkUnduh: "https://www.mediafire.com/file/886jlmsfun070hc/Archeologeist.mcpack/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "2.51 MB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 16,
+    nama: "Protection Blocks",
+    pembuat: "Voxz Official",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/9la01n.jpg",
+    linkUnduh: "https://www.mediafire.com/file/38y3em1iwkzlfyn/Protection_Blocks_%2528addon%2529.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "609.59 KB",
+    deskripsi: "No Captions."
+  },
+  {
+    id: 17,
+    nama: "Dino Labs",
+    pembuat: "Kuona",
+    kategori: "Add-on",
+    gambar: "https://files.catbox.moe/cjiidc.jpg",
+    linkUnduh: "https://www.mediafire.com/file/jsbfggd3dlszt4d/Dino_Labs_By_Street_Studios.mcaddon/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "17.61 MB",
+    deskripsi: "DINOSAURUS DI MINECRAFT🦖"
+  },
+{
+    id: 18,
+    nama: "Void Weapons",
+    pembuat: "Kuona",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/0w2ng3.jpg",
+    linkUnduh: "https://www.mediafire.com/file/0y6sijox435vfio/Void_Weapons_By_ZYDAM_%2528world_template%2529.mctemplate/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "3.79 MB",
+    deskripsi: "SENJATA OP DI MINECRAFT🌑"
+  },
+{
+    id: 19,
+    nama: "Ether Dimension",
+    pembuat: "Kuona",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/o33srg.jpg",
+    linkUnduh: "https://www.mediafire.com/file/qng8i23md6o6q7s/ETHER_DIMENSION_II_%2528world_template%2529.mctemplate/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "119.01 MB",
+    deskripsi: "DIMENSI SURGA DI MINECRAFT✨"
+  },
+{
+    id: 30,
+    nama: "Bank Rush",
+    pembuat: "Kuona",
+    kategori: "Map",
+    gambar: "https://files.catbox.moe/9fip9c.jpg",
+    linkUnduh: "https://www.mediafire.com/file/q7hwafveb5h6a1m/Bank_Rush_%2528world_template%2529.mctemplate/file",
+    kompatibel: "MCPE 1.21 - 1.26+",
+    ukuran: "3.55 MB",
+    deskripsi: "PERAMPOKAN DI MINECRAFT🪙"
+  }
+];
